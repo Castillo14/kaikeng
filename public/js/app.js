3a6a7fddd42ea5da31eb7ec4392d17b88cc082a3
Castillo14 <castillomaryjoy14@gmail.com>
2021-09-02 18:17:16 +0800

    public

--- public/js/app.js
+++ public/js/app.js
@@ -515,8 +515,8 @@ eval("// removed by extract-text-webpack-plugin//# sourceURL=[module]\n//# sourc
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\laragon\www\kaikenghelp\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\laragon\www\kaikenghelp\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\laragon\www\kaikeng\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\laragon\www\kaikeng\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
