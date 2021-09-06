7358f3a99831f4b46aaa59126ba9467b5a870a41
Renier Trenuela <rtrenuela@objectbright.com>
2021-09-06 13:03:49 +0800

    update meta

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -3,8 +3,11 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content=""/>
-    <meta name="author" content=""/>
+    <meta property="og:url"                content="{{ route('landing') }}" />
+    <meta property="og:type"               content="Foundation" />
+    <meta property="og:title"              content="{{ config('app.name', 'Laravel') }}" />
+    <meta property="og:description"        content="" />
+    <meta property="og:image"              content="{{ asset('images/CARLO.png') }}" />
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
