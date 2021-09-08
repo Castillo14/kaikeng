81f2331226f99123fd6b72b5f8f44aa767834ab3
Renier Trenuela <rtrenuela@objectbright.com>
2021-09-08 12:08:37 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -33,26 +33,26 @@ Route::group(['middleware' => 'auth'], function () {
         Route::get('/preview/{id}', [HomeController::class, 'preview'])->name('home.preview');
     });
 
-    Route::prefix('delegations')->group(function () {
-        Route::get('/', [DelegationController::class, 'index'])->name('delegations');
-        Route::post('/table', [DelegationController::class, 'table'])->name('delegations.table');
-        Route::get('/create', [DelegationController::class, 'create'])->name('delegations.create');
-        Route::post('/store', [DelegationController::class, 'store'])->name('delegations.store');
-    });
-
     Route::prefix('agencies')->group(function () {
-        Route::get('/', [AgencyController::class, 'index'])->name('agencies');
+        Route::get('/', [AgencyController::class, 'index'])->name('agencies')->middleware(['can:admin']);
         Route::post('/store', [AgencyController::class, 'store'])->name('agency.store');
         Route::get('/d/{id}', [AgencyController::class, 'destroy'])->name('agency.destroy');
     });
 
     Route::prefix('user')->group(function () {
-        Route::get('/', [UserController::class, 'index'])->name('users');
+        Route::get('/', [UserController::class, 'index'])->name('users')->middleware(['can:admin']);
         Route::get('/table', [UserController::class, 'table'])->name('users.table');
         Route::post('/register', [UserController::class, 'register'])->name('users.register');
         Route::get('/create', [UserController::class, 'create'])->name('users.create');
         Route::get('/show/{id}', [UserController::class, 'show'])->name('users.show');
         Route::post('/update', [UserController::class, 'update'])->name('users.update');
         Route::get('/destroy/{id}', [UserController::class, 'destroy'])->name('users.destroy');
+    });
+
+    Route::prefix('delegations')->group(function () {
+        Route::get('/', [DelegationController::class, 'index'])->name('delegations')->middleware(['can:admin']);
+        Route::post('/table', [DelegationController::class, 'table'])->name('delegations.table');
+        Route::get('/create', [DelegationController::class, 'create'])->name('delegations.create');
+        Route::post('/store', [DelegationController::class, 'store'])->name('delegations.store');
     });
 });
