0523bef527042de770490f09e600528382fda844
Renier Trenuela <rtrenuela@objectbright.com>
2021-09-09 14:16:28 +0800

    fix

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -28,11 +28,11 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         Gate::define('admin', function (User $user) {
-            return $user->role === 1;
+            return $user->role == 1;
         });
 
         Gate::define('agency', function (User $user) {
-            return $user->role === 2;
+            return $user->role == 2;
         });
     }
 }
